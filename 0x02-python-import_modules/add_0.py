7826b1d97929f24e4afe1a89acbc424b1f8235a7
tbir8256 <tbir8256@gmail.com>
2022-08-25 17:39:11 -0700

    0-add

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+    Args:
+    a: first integer
+    b: second integer
+    Retruns:
+    The return vvalue. a + b
+    """
+    return (a + b)
