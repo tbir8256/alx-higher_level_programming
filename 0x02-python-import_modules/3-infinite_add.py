fc25cc984635d69b17ca7c11d6693c8592d1500f
tbir8256 <tbir8256@gmail.com>
2022-08-31 17:38:14 -0700

    Fix-03

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
+    from sys import argv
     numarg = 0
-    for i in sys.argv[1:]:
-        numarg = numarg + int(i)
+    for num in argv[1:]:
+        numarg = numarg + int(num)
         print(numarg)
