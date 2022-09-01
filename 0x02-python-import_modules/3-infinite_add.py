57f2b71df104273ef3e672140077eed178ad6695
tbir8256 <tbir8256@gmail.com>
2022-08-31 17:52:19 -0700

    fix_03

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-
-import sys
-    def main():
-        result = 0
-        for i in range(1, len(sys.argv)):
-            result += int(sys.argv[i])
-            print(result)
+def add_arg(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d}".format(n))
+        return
+    else:
+    i = 1
+    add = 0
+    while i <= n:
+        add += int(argv[i])
+        i += 1
+        print("{:d}".format(add))
 if __name__ == "__main__":
-            main()
+    import sys
+    add_art(sys.argv)
