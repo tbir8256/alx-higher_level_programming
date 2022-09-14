c23198cda698dca602c560990917f03bd378f542
tbir8256 <tbir8256@gmail.com>
2022-09-13 18:58:20 -0700

    Fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""" A square class."""
+
 
 class Square:
+    """Derives a square. """
     def __init__(self, size):
+        """Initializes attributes. """
         self.__size = size
