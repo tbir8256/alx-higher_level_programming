8bcc13dd43641caf8f5b8011e74dcd789d0e7cdf
tbir8256 <tbir8256@gmail.com>
2022-09-13 19:51:54 -0700

    fix

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,7 +6,7 @@ class Square:
     """Derives a square."""
 
     def __init__(self, size=0):
-    """Initialize a new square."""
+        """Initialize a new square."""
 
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
