bdf390e3483039b859bf5e08d15a74ec160acec1
tbir8256 <tbir8256@gmail.com>
2022-09-13 19:17:36 -0700

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,19 +3,17 @@
 
 
 class Square:
-    """Derives a square. """
-
+    """Derives a square."""
     def __init__(self, size=0):
-        """Initializes the data. """
-
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-
-        def area(self):
-        """Calculates the area of a square
-        Returns: the area of the square"""
-
-        return (self.__size ** 2)
+    """Initialize a new square.
+    Args:
+    size (int): The size of the new square.
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    self.__size = size
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
