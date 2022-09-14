6d95b7424ba3d5042b0d0d86afacc374dd5291ed
tbir8256 <tbir8256@gmail.com>
2022-09-13 19:08:07 -0700

    3-square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -8,7 +8,7 @@ class Square:
         """Initializes the data. """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if (size < 0):
+        elif (size < 0):
             raise ValueError("size must be >= 0")
         self.__size = size
 
