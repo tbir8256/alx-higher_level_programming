a1c65380f8bd8035c703ed91ae181e92af885dee
tbir8256 <tbir8256@gmail.com>
2022-09-16 20:49:42 -0700

    readme

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -11,7 +11,7 @@ class Square:
         Args:
             size (int):Size of the square.
             """
-    if isinstance(size, int) is False:
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
