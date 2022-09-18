d3cb3641f282694ee79cab1669150be6425df1f2
tbir8256 <tbir8256@gmail.com>
2022-09-18 02:53:55 -0700

    fix-00

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,9 +4,9 @@ def safe_print_list(my_list=[], x=0):
     a = 0
     for i in range(x):
         try:
-            print"{}".format(my_list[i]), end='')
+            print("{}".format(my_list[i]), end='')
             a += 1
-    except IndexError:
-        break
-    print()
-    return a
+        except IndexError:
+            break
+        print()
+        return a
