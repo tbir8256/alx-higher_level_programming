6ca8231e7802aed28cd08923d0bdd24c101dba06
tbir8256 <tbir8256@gmail.com>
2022-09-18 04:31:45 -0700

    0-safe_print_list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def safe_pritn_list(my_list=[], x=0):
+def safe_print_list(my_list=[], x=0):
     a = 0
-    for i in ranbe(x):
+    for i in range(x):
         try:
-            print(my_list=[i], end="")
+            print(my_list[i], end="")
             a += 1
         except IndexError:
             break
