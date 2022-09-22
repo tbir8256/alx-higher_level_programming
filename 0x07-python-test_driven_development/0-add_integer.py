48e5fbe7bb8a61f67300984748f5c474a62a8f85
tbir8256 <tbir8256@gmail.com>
2022-09-21 20:09:29 -0700

    0-add_integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -13,67 +13,62 @@ these numbers can be integers or floats.
 
 
 def add_integer(a, b=98):
+        
+    """Adds two numbers
 
-        """Adds two numbers
+    Performs the addition between two numbers.
 
-            Performs the addition between two numbers.
+    Args:
 
-                Args:
+        a (:obj:`int, float`): The first number.
 
-                        a (:obj:`int, float`): The first number.
+        b (:obj:`int, float`, optional): The second number.
 
-                                b (:obj:`int, float`, optional): The second number.
+    Returns:
 
-                                    Returns:
+        int: The result of the addition.
 
-                                            int: The result of the addition.
-
-                                                """
-
-                                                    if type(a) not in (int, float):
-
-                                                                raise TypeError('a must be an integer')
-
-
-
-                                                                if type(b) not in (int, float):
-
-                                                                            raise TypeError('b must be an integer')
+    """
+    
+    if type(a) not in (int, float):
+            
+        raise TypeError('a must be an integer')
 
 
 
-                                                                            a = convert_to_int(a)
-
-                                                                                b = convert_to_int(b)
-
-                                                                                    return a + b
+    if type(b) not in (int, float):
+            
+        raise TypeError('b must be an integer')
 
 
 
+    a = convert_to_int(a)
+    
+    b = convert_to_int(b)
+    
+    return a + b
+def convert_to_int(num):
+        
+    """Cast the data type of num parameter
+
+    Convert a float number to a integer number
+
+    Args:
+
+        num (:obj:`int, float`): The number to cast.
+
+    Returns:
+
+        int: The number casted to integer.
+
+    """
+    
+    if type(num) is float:
+            
+        num = int(num)
+        
+        return num
 
 
-                                                                                def convert_to_int(num):
 
-                                                                                        """Cast the data type of num parameter
-
-                                                                                            Convert a float number to a integer number
-
-                                                                                                Args:
-
-                                                                                                        num (:obj:`int, float`): The number to cast.
-
-                                                                                                            Returns:
-
-                                                                                                                    int: The number casted to integer.
-
-                                                                                                                        """
-
-                                                                                                                            if type(num) is float:
-
-                                                                                                                                        num = int(num)
-
-                                                                                                                                                return num
-
-
-
-                                                                                                                                                return num
+    return num
