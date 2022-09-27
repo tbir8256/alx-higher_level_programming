b3a48f124f254a23d75d6da4e504279a90418f0e
tbir8256 <tbir8256@gmail.com>
2022-09-26 19:00:45 -0700

    10-square

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+
+"""Module for a lookup function"""
+
+
+
+
+
+def lookup(obj):
+    
+    """
+
+        Return list of available object and methods
+
+        in an object
+
+    """
+    
+    return dir(obj)
