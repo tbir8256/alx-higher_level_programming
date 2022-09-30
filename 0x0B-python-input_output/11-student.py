82bc702b1cfe8a5e7fe1f781df8b1eb2470e5578
tbir8256 <tbir8256@gmail.com>
2022-09-29 20:03:26 -0700

    11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -13,14 +13,14 @@ class Student:
     Public method to_json().
     Public method reload_from_json().
     """
-    
+
     def __init__(self, first_name, last_name, age):
         """Initializes the Student instance."""
-        
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        
+
     def to_json(self, attrs=None):
         """Retrieves a dictionary representation
         of a Student instance.
@@ -28,7 +28,7 @@ class Student:
             - attrs: list of attributes
         Returns: the dict representation of the instance.
         """
-        
+
         my_dict = dict()
         if attrs and all(isinstance(x, str) for x in attrs):
             for x in attrs:
@@ -36,7 +36,7 @@ class Student:
                     my_dict.update({x: self.__dict__[x]})
             return my_dict
         return self.__dict__
-    
+
     def reload_from_json(self, json):
         """Replaces all attributes of the Student instance.
         Args:
