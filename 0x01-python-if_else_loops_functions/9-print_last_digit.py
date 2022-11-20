adac8f101c7cb8e145ef650da67558f70da31395
tbir8256 <tbir8256@gmail.com>
2022-11-20 01:27:19 -0800

    9-p

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-def uppercase(str):
-    case = 0
-    for i in str:
-        if ord(i) >= ord('a') and ord(i) <= ord('z'):
-            case = 32
-        else:
-            case = 0
-     print('{:c}'.format(ord(i) - case), end='')
-     print()
+def print_last_digit(number):
+    if number < 0:
+        number = number * -1
+        num = number % 10
+        print("{}".format(num), end='')
+        return num
