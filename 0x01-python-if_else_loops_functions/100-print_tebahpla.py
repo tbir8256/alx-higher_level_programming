a0e9df0fbe6d23147d6cda2d369dcc6e6d336b9d
tbir8256 <tbir8256@gmail.com>
2022-11-20 01:07:36 -0800

    fix10

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
-for i in reversed(range(97, 123)):
-    if i % 2 == 0:
-        print('{:c}'.format(i), end='')
+for i in range(122, 96, -1):
+    if i % 2 != 0:
+        print("{:c}".format(i - 32), end='')
     else:
-        print('{:c}'.format(i - 32), end='')
+        print("{:c}".format(i), end='')
