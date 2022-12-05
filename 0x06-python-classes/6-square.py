419edf0114a4b5dfdaf2ec55d2d8c2b4c74bb1a0
tbir8256 <tbir8256@gmail.com>
2022-12-05 08:16:08 -0800

    6.

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,71 +1,116 @@
 #!/usr/bin/python3
-"""
-Defines a class Rectangle
-"""
+"""a square class"""
 
 
-class Rectangle:
-    """Representation of a rectangle"""
+class Square:
+    """Derives a square """
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes the data
 
-    number_of_instances = 0
+        Args:
 
-    def __init__(self, width=0, height=0):
-        """Initializes the rectangle"""
-        self.width = width
-        self.height = height
-        Rectangle.number_of_instances += 1
+            size (int): size of the square
 
-    def __del__(self):
-        """prints a string when an instance has been deleted"""
-        print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
+            position (tuple): two positive integers
 
-    @property
-    def width(self):
-        """getter for the private instance attribute width"""
-        return self.__width
+        Note:
 
-    @width.setter
-    def width(self, value):
-        """setter for the private instance attribute width"""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+            Do not include the `self` parameter in the ``Args`` section.
 
-    @property
-    def height(self):
-        """getter for the private instance attribute height"""
-        return self.__height
+        Raises:
 
-    @height.setter
-    def height(self, value):
-        """setter for the private instance attribute height"""
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+            TypeError: when `size` isn't an integer
+
+            ValueError: `size` is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+        if not isinstance(position, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len(position) != 2 or not all(isinstance(v, int) for v in position):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not all(num >= 0 for num in position):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = position
 
     def area(self):
-        """returns the area of the rectangle"""
-        return self.__width * self.__height
+        """Calculates the area of a square
 
-    def perimeter(self):
-        """returns the perimeter of the rectangle"""
-        if self.__width == 0 or self.__height == 0:
-            return 0
-        return (self.__width * 2) + (self.__height * 2)
+        Returns: the area of the square
+        """
 
-    def __str__(self):
-        """returns printable string representation of the rectangle"""
-        string = ""
-        if self.__width != 0 and self.__height != 0:
-            string += "\n".join("#" * self.__width
-                                for j in range(self.__height))
-        return string
+        return (self.__size ** 2)
 
-    def __repr__(self):
-        """returns a string representation of the rectangle for reproduction"""
-        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+    @property
+    def size(self):
+        """Retrieves the value of `size`"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the value of `size`
+
+        Args:
+
+            value (int): value to be set to `size`
+
+        Raise:
+
+            TypeError: when `value` isn't an integer
+
+            ValueError: `value` is less than 0
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if (value < 0):
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    @property
+    def position(self):
+        """Retrieves `position` value """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Sets the value of `position`
+
+        Args:
+
+            value (int): value to be set to `position` attribute
+
+        Raise:
+
+            TypeError: position isn't a tupple or doesn't contain 2
+
+                       elements or has negative integers
+
+        """
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len(value) != 2 or not all(isinstance(v, int) for v in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not all(num >= 0 for num in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        self.__position = value
+
+    def my_print(self):
+        """Prints a square to stdout using #"""
+        if self.__size == 0:
+            print()
+            return
+
+        [print() for i in range(self.position[1])]
+        for i in range(self.__size):
+            for i in range(self.__position[0]):
+                print(" ", end="")
+            for i in range(self.__size):
+                print("#", end="")
+            print()
