ea323f08aededb3d34a9b7d067c0642fa6adedf7
tbir8256 <tbir8256@gmail.com>
2023-03-09 01:14:29 +0000

    fix101

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,15 @@
 #!/usr/bin/node
 
 const dict = require('./101-data').dict;
-const newDict = {};
 
-Object.keys(dict).map(function (key) {
-  if (!Array.isArray(newDict[dict[key]])) {
-    newDict[dict[key]] = [];
+const entries = Object.entries(dict);
+const newDict = {};
+for (const [key, value] of entries) {
+  if (value in newDict) {
+    newDict[value].push(key);
+  } else {
+    newDict[value] = [key];
   }
-  newDict[dict[key]].push(key);
-});
+}
+
+console.log(newDict);
